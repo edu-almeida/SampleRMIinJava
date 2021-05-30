c01a393a56781973bda524e118da1c2dc0d4c049
edu-almeida <edualmeida.a7@gmail.com>
2021-05-29 23:11:07 -0300

    1.0.3 Inicialização de packages e classes

--- src/RMI.java
+++ src/RMI.java
@@ -1,3 +1,17 @@
-public class RMI {
+import rmi.Server;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
+public class RMI {
+    public static void main(String[] args) {
+        try {
+            Server server = new Server();
+            String location = "//localhost/service";
+            Naming.rebind(location, server);
+        } catch (RemoteException | MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
 }
